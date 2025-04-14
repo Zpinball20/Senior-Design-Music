37260939d0342b680c803eb2699b9f18daab2692
Zpinball20 <zsf4@pitt.edu>
2025-04-14 11:56:38 -0400

    Fully functional without serial communication

--- main-files/pythonToArduino.py
+++ main-files/pythonToArduino.py
@@ -2,7 +2,7 @@ import serial
 import time
 from PyQt6.QtCore import QObject, pyqtSignal as Signal, pyqtSlot as Slot, QTimer
 
-arduino = serial.Serial(port='COM5',   baudrate=115200, timeout=.1) #port name is subject to change
+#arduino = serial.Serial(port='COM5',   baudrate=115200, timeout=.1) #port name is subject to change
 
 class py_to_arduino(QObject):
     raw_arduino_data = Signal(list)
@@ -15,24 +15,25 @@ class py_to_arduino(QObject):
         self.octave = ""
         self.beatLengthRounded = 0.0
 
+
     @Slot(bool)
     def record_audio(self, input: bool):
-        arduino.write(bytes(str(input), 'utf-8'))
+        #arduino.write(bytes(str(input), 'utf-8'))
 
         if(input == True):
             self.timer.start(500)
             print("Now recording")
 
         if(input == False):
-            self.timer.start(500)
-            data = self.parseData()
+            #time.sleep(10)
+            #data = self.parseData()
             #print(dataArd)
-            #data = [
-            #    {"pitch": "C", "accidental": "", "octave": 4, "duration": 1},
-            #    {"pitch": "", "accidental": "", "octave": 4, "duration": 2},
-            #    {"pitch": "E", "accidental": "", "octave": 4, "duration": 0.5},
-            #    {"pitch": "F", "accidental": "", "octave": 4, "duration": 0.5}
-            #]
+            data = [
+                {"pitch": "C", "accidental": "", "octave": 4, "duration": 1},
+                {"pitch": "", "accidental": "", "octave": 4, "duration": 2},
+                {"pitch": "E", "accidental": "", "octave": 4, "duration": 0.5},
+                {"pitch": "F", "accidental": "", "octave": 4, "duration": 0.5}
+            ]
 
             self.raw_arduino_data.emit(data)
             print("End Recording")
@@ -41,11 +42,13 @@ class py_to_arduino(QObject):
         data = []  # Initialize an empty list to store the parsed data
 
         while True:
-            note = arduino.readline().decode('utf-8').strip()  # Read note
-            duration = arduino.readline().decode('utf-8').strip()  # Read duration
+            #note = arduino.readline().decode('utf-8').strip()
+            #duration = arduino.readline().decode('utf-8').strip()
 
-            print(note)
-            print(duration)
+            #print(note)
+            #print(duration)
+            note = "blah"
+            duration = "blah"
 
             if not note or not duration:  # Break out of loop if no data
                 break
